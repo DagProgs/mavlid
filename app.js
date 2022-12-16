f7ace68ca0aec3843130a12e3a7f5e0d6945d69a
DagProgs <114062397+DagProgs@users.noreply.github.com>
2022-12-16 21:05:46 +0300

    mavlidLezgi

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-}
+window.addEventListener('load', () => {
+
+    if ('serviceWorker' in navigator){
+
+        navigator.serviceWorker.register('serviceWorker.js')
+            .then(registration => {
+                console.log('Service worker successfully registered', registration);
+            })
+            .catch(error => {
+                console.log('Service worker registration failed', error);
+            });
+    }
+});
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,7 @@
-const staticDevCoffee = "dev-coffee-site-v1"
-const assets = [
+const staticCacheName = 'static-cache-v0';
+const dynamicCacheName = 'dynamic-cache-v0';
+
+const staticAssets = [
   "/",
   "/index.html",
   "/assets/style/style.css",
@@ -31,22 +33,51 @@ const assets = [
   "/assets/mavlid/23.html",
   "/assets/mavlid/24.html",
   "/assets/mavlid/25.html",
-  "/assets/mavlid/26.html",
+  "/assets/mavlid/26.html"
   
 ]
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
+self.addEventListener('install', async event => {
+    const cache = await caches.open(staticCacheName);
+    await cache.addAll(staticAssets);
+    console.log('Service worker has been installed');
+});
 
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  )
-})
+self.addEventListener('activate', async event => {
+    const cachesKeys = await caches.keys();
+    const checkKeys = cachesKeys.map(async key => {
+        if (![staticCacheName, dynamicCacheName].includes(key)) {
+            await caches.delete(key);
+        }
+    });
+    await Promise.all(checkKeys);
+    console.log('Service worker has been activated');
+});
+
+self.addEventListener('fetch', event => {
+    console.log(`Trying to fetch ${event.request.url}`);
+    event.respondWith(checkCache(event.request));
+});
+
+async function checkCache(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || checkOnline(req);
+}
+
+async function checkOnline(req) {
+    const cache = await caches.open(dynamicCacheName);
+    try {
+        const res = await fetch(req);
+        await cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        const cachedRes = await cache.match(req);
+        if (cachedRes) {
+            return cachedRes;
+        } else if (req.url.indexOf('.html') !== -1) {
+            return caches.match('offline.html');
+        } else {
+            return caches.match('images/no-image.jpg');
+        }
+    }
+}
