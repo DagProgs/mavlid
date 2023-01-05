a052c92f41ee4768cbf51cd50a72b3b8fef3e779
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-01-05 17:51:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
 
     if ('serviceWorker' in navigator){
 
-        navigator.serviceWorker.register('sw.js')
+        navigator.serviceWorker.register('pwabuilder-sw.js')
             .then(registration => {
                 console.log('Service worker successfully registered', registration);
             })
