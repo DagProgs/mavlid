2b824299326ddb3a2a3e18a42eff5734f00d7c3f
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-01-06 08:04:00 +0300

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'static-cache-v18';
-const dynamicCacheName = 'dynamic-cache-v18';
+const staticCacheName = 'static-cache-v19';
+const dynamicCacheName = 'dynamic-cache-v19';
 
 // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
@@ -8,7 +8,7 @@ const CACHE = "pwabuilder-offline-page";
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const offlineFallbackPage = "offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
