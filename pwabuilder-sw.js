06656942239a877d832b52af9ff18f2a6eb54353
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-03-09 14:24:41 +0300

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,5 +1,5 @@
-const PRECACHE = 'precache-v6';
-const RUNTIME = 'runtime-v1';
+const PRECACHE = 'precache-v7';
+const RUNTIME = 'runtime';
  
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
