b5338b1610afe3d38c364877cc64fc9f1a33648b
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-03-09 10:33:01 +0300

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,64 +1,70 @@
-const staticCacheName = 'site-static-v1.1.3';
-const dynamicCacheName = 'site-dynamic-v1.1';
+const staticCacheName = 'site-static-v1';
+
 const assets = [
-'/',
-'/index.html'
+
+  '/',
+
+  '/index.html'
 ];
 
-// cache size limit function
-const limitCacheSize = (name, size) => {
-caches.open(name).then(cache => {
-cache.keys().then(keys => {
-if(keys.length > size){
-cache.delete(keys[0]).then(limitCacheSize(name, size));
-}
-});
-});
-};
 
-// install event
+
+// событие install
+
 self.addEventListener('install', evt => {
-//console.log('service worker installed');
-evt.waitUntil(
-caches.open(staticCacheName).then((cache) => {
-console.log('caching shell assets');
-cache.addAll(assets);
-})
-);
+
+  evt.waitUntil(
+
+    caches.open(staticCacheName).then((cache) => {
+
+      console.log('caching shell assets');
+
+      cache.addAll(assets);
+
+    })
+
+  );
+
 });
 
-// activate event
+
+
+// событие activate
+
 self.addEventListener('activate', evt => {
-//console.log('service worker activated');
-evt.waitUntil(
-caches.keys().then(keys => {
-//console.log(keys);
-return Promise.all(keys
-.filter(key => key !== staticCacheName && key !== dynamicCacheName)
-.map(key => caches.delete(key))
-);
-})
-);
+
+  evt.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(keys
+
+        .filter(key => key !== staticCacheName)
+
+        .map(key => caches.delete(key))
+
+      );
+
+    })
+
+  );
+
 });
 
-// fetch event
+
+
+// событие fetch
+
 self.addEventListener('fetch', evt => {
-if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
-evt.respondWith(
-caches.match(evt.request).then(cacheRes => {
-return cacheRes || fetch(evt.request).then(fetchRes => {
-return caches.open(dynamicCacheName).then(cache => {
-cache.put(evt.request.url, fetchRes.clone());
-// check cached items size
-limitCacheSize(dynamicCacheName, 15);
-return fetchRes;
-})
-});
-}).catch(() => {
-if(evt.request.url.indexOf('.html') > -1){
-return caches.match('/offline.html');
-}
-})
-);
-}
+
+  evt.respondWith(
+
+    caches.match(evt.request).then(cacheRes => {
+
+      return cacheRes || fetch(evt.request);
+
+    })
+
+  );
+
 });
