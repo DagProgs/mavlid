76c11c9e40bab7dfaf67dabb1fb92d94da3c9db6
DagProgs <114062397+DagProgs@users.noreply.github.com>
2024-02-21 17:41:52 +0300

    update

--- sw-workbox.js
+++ sw-workbox.js
@@ -19,7 +19,60 @@ workbox.core.clientsClaim();
 // PRECACHING
 
 // We inject manifest here using "workbox-build" in workbox-build-inject.js
-workbox.precaching.precacheAndRoute([]);
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "index.html",
+    "revision": "48491aea8e6f7a7dc5a5ebabcb966b53"
+  },
+  {
+    "url": "css/style.css",
+    "revision": "38e0d16bda5ef2257aa2551eb69acda8"
+  },
+  {
+    "url": "js/jquery-3.6.0.min.js",
+    "revision": "8fb8fee4fcc3cc86ff6c724154c49c42"
+  },
+  {
+    "url": "js/script.js",
+    "revision": "e04d8508de4258e983c04f94fcea56fc"
+  },
+  {
+    "url": "img/icons/icon-128x128.png",
+    "revision": "c1fdb4f328dc224c31d4f63b37a2d7eb"
+  },
+  {
+    "url": "img/icons/icon-144x144.png",
+    "revision": "fbf62408a29bafea9714e5006212cdbc"
+  },
+  {
+    "url": "img/icons/icon-152x152.png",
+    "revision": "480c47876f2576e4ea51c6e6f6b3a0e1"
+  },
+  {
+    "url": "img/icons/icon-192x192.png",
+    "revision": "661e7275a650d6a3616d38e98bd09219"
+  },
+  {
+    "url": "img/icons/icon-384x384.png",
+    "revision": "15230a15012db5dfc25c8756191f8ded"
+  },
+  {
+    "url": "img/icons/icon-48x48.png",
+    "revision": "34950ff63a4a4de30783fa922faee9c2"
+  },
+  {
+    "url": "img/icons/icon-512x512.png",
+    "revision": "28dca3664bb91e23c7a8ad2d0d8194d7"
+  },
+  {
+    "url": "img/icons/icon-72x72.png",
+    "revision": "bcd59f0dc93d37c8e8f963471f39028d"
+  },
+  {
+    "url": "img/icons/icon-96x96.png",
+    "revision": "cce9088080585a31862e0fe7d70d306f"
+  }
+]);
 
 // RUNTIME CACHING
 
